9c522e63590b59b048dfec7074ad71b9965fe4d6
Cookie McCormick <muntananuchat@gmail.com>
2019-02-17 19:45:23 -0500

    Done.

--- db/migrate/20150915202704_create_owners.rb
+++ db/migrate/20150915202704_create_owners.rb
@@ -1,11 +1,8 @@
 class CreateOwners < ActiveRecord::Migration
   def up
     create_table :owners do |t|
-      t.string :name
+      t.string :name, null: false
+      t.timestamps null: false
     end
-  end
-  
-  def down
-    drop_table :owners
   end
 end
