38ef9a4c088d43befd5784d0d88a9b9d1186db72
alex6851 <alex.baker685@gmail.com>
2019-09-02 19:38:58 -0700

    Done.

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -3,7 +3,7 @@ class CreateCats < ActiveRecord::Migration
     create_table :cats do |t|
       t.string :name
       t.integer :age
-      t.string :breed
+      t.string  :breed
     end
   end
   
